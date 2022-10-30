2eac68de3800a69a271c78e0d8af284f7bb99b53
Wilmer Peñafiel <109515122+wilmerpe@users.noreply.github.com>
2022-10-30 14:36:25 +0100

    Update EjercicioConCondicionales2.java

--- src/EjercicioConCondicionales2.java
+++ src/EjercicioConCondicionales2.java
@@ -7,11 +7,11 @@ public class EjercicioConCondicionales2 {
         numero2 = Integer.parseInt(JOptionPane.showInputDialog("Defina el valor de número2"));
 
         if(numero1 > numero2){
-        JOptionPane.showMessageDialog(null,"Número1 es mayor a número2");
+        JOptionPane.showMessageDialog(null,"El número mayor es: " + numero1);
         }else if(numero1 < numero2){
-            JOptionPane.showMessageDialog(null,"Número1 es menor a número2");
+            JOptionPane.showMessageDialog(null,"El número mayor es: " + numero2);
         }else{
-            JOptionPane.showMessageDialog(null,"Número1 y número2 son iguales");
+            JOptionPane.showMessageDialog(null,"Los dos números son iguales");
         }
     }
 }
